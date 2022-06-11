62558c6016400a2f9406476b7de5d647cc0bd60e
Roduan Kareem Aldeen <roduan98@gmail.com>
2022-06-11 16:22:50 +0300

    disable vuetify sass for now

--- resources/webpack.config.js
+++ resources/webpack.config.js
@@ -8,39 +8,39 @@ module.exports = {
     }),
     new ESLintPlugin()
   ],
-  module: {
-    rules: [
-      // SASS has different line endings than SCSS
-      // and cannot use semicolons in the markup
-      {
-        test: /\.sass$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              // This is the path to your variables
-              additionalData: "@import './resources/scss/variables.scss'"
-            },
-          },
-        ],
-      },
-      // SCSS has different line endings than SASS
-      // and needs a semicolon after the import.
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              additionalData: "@import './resources/scss/variables.scss';"
-            },
-          },
-        ],
-      },
-    ],
-  },
+  // module: {
+  //   rules: [
+  //     // SASS has different line endings than SCSS
+  //     // and cannot use semicolons in the markup
+  //     {
+  //       test: /\.sass$/,
+  //       use: [
+  //         'vue-style-loader',
+  //         'css-loader',
+  //         {
+  //           loader: 'sass-loader',
+  //           options: {
+  //             // This is the path to your variables
+  //             additionalData: "@import './resources/scss/variables.scss'"
+  //           },
+  //         },
+  //       ],
+  //     },
+  //     // SCSS has different line endings than SASS
+  //     // and needs a semicolon after the import.
+  //     {
+  //       test: /\.scss$/,
+  //       use: [
+  //         'vue-style-loader',
+  //         'css-loader',
+  //         {
+  //           loader: 'sass-loader',
+  //           options: {
+  //             additionalData: "@import './resources/scss/variables.scss';"
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
 }
--- resources/webpack.mix.js
+++ resources/webpack.mix.js
@@ -11,17 +11,6 @@ const mix = require('laravel-mix')
  |
  */
 
-// eslint-disable-next-line no-undef
-Mix.listen('configReady', config => {
-  const scssRule = config.module.rules.find(r => r.test.toString() === /\.scss$/.toString())
-  const scssOptions = scssRule.oneOf[0].use.find(l => l.loader.search('sass-loader') !== -1).options
-  scssOptions.additionalData = '@import "./resources/scss/variables.scss";'
-
-  const sassRule = config.module.rules.find(r => r.test.toString() === /\.sass$/.toString())
-  const sassOptions = sassRule.oneOf[0].use.find(l => l.loader.search('sass-loader') !== -1).options
-  sassOptions.additionalData = '@import "./resources/scss/variables.scss"'
-})
-
 mix.js('resources/js/app.js', 'public/js')
   .vue()
   .postCss('resources/css/app.css', 'public/css', [require('postcss-import'), require('autoprefixer')])
