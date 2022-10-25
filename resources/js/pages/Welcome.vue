f9191ee0a62f3bddbf2d235f047b7eba20532d0d
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2022-10-25 17:18:37 +0300

    move welcome page to the root of pages folder

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    Hello world!
+  </v-container>
+</template>
+
+<script>
+import AppLayout from '../layouts/AppLayout.vue'
+
+export default {
+  name: 'WelcomeView',
+  layout: AppLayout,
+}
+</script>
