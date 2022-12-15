96afe195b4d84e598e8c2df29d7aa4dd1ef3bac2
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2022-12-15 19:00:40 +0300

    make authentication background size as cover
    Took 7 minutes

--- resources/js/components/Auth/AuthenticationCard.vue
+++ resources/js/components/Auth/AuthenticationCard.vue
@@ -3,7 +3,7 @@
     fluid
     justify-center
     fill-height
-    class="login"
+    class="authentication"
   >
     <v-row
       align="center"
@@ -31,7 +31,8 @@
 </template>
 
 <style>
-.login {
+.authentication {
   background: url('https://images.unsplash.com/photo-1483354483454-4cd359948304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80');
+  background-size: cover;
 }
 </style>
